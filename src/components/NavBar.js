c75d1812cad1c98cb635fad57efc09751de385b9
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-10-26 19:18:13 +1300

    Navbar and styling

--- src/components/NavBar.js
+++ src/components/NavBar.js
@@ -1,21 +1,54 @@
 import React from 'react';
+import { useState } from 'react';
 import { NavLink } from 'react-router-dom';
+import { ReactComponent as CloseMenu } from '../assets/x.svg';
+import { ReactComponent as MenuIcon } from '../assets/menu.svg';
 
 export default function NavBar() {
+  const [click, setClick] = useState(false);
+  const handleClick = () => setClick(!click);
+  const closeMobileMenu = () => setClick(false);
   return (
-    <header className='header'>
-      <div>
-        <nav>
+    <div className='header'>
+      <div className='logo-nav'>
+        <div className='logo-container'>
           <NavLink to='/' exact>
-            <img className='Logo' src='HNG.png' alt='HNG Logo' />
+            <img className='logo' src='HNG.png' alt='HNG Logo' />
           </NavLink>
-          <NavLink to='/cliches'>101 Common Cliches</NavLink>
-          <NavLink to='/additionalcliches'>101 Additional Cliches</NavLink>
-          <NavLink to='/mymind'>My Mind Is Out To Get Me</NavLink>
-          <NavLink to='/sobercards'>Sober Cards</NavLink>
-          <NavLink to='/bbcards'>Big Book Cards</NavLink>
-        </nav>
+        </div>
+        <div className={click ? 'nav-options active' : 'nav-options'}>
+          <NavLink to='/cliches' className='option' onClick={closeMobileMenu}>
+            101 Common Cliches
+          </NavLink>
+          <NavLink
+            to='/additionalcliches'
+            className='option'
+            onClick={closeMobileMenu}
+          >
+            101 Additional Cliches
+          </NavLink>
+          <NavLink to='/mymind' className='option' onClick={closeMobileMenu}>
+            My Mind Is Out To Get Me
+          </NavLink>
+          <NavLink
+            to='/sobercards'
+            className='option'
+            onClick={closeMobileMenu}
+          >
+            Sober Cards
+          </NavLink>
+          <NavLink to='/bbcards' className='option' onClick={closeMobileMenu}>
+            Big Book Cards
+          </NavLink>
+        </div>
       </div>
-    </header>
+      <div className='mobile-menu' onClick={handleClick}>
+        {click ? (
+          <CloseMenu className='menu-icon' />
+        ) : (
+          <MenuIcon className='menu-icon' />
+        )}
+      </div>
+    </div>
   );
 }
