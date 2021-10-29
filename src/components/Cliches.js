c65d19def62b221656026281af97a6b571079399
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-10-29 21:00:06 +1300

    Started cliche page

--- src/components/Cliches.js
+++ src/components/Cliches.js
@@ -1,5 +1,24 @@
 import React from 'react';
 
 export default function Cliches() {
-  return <h1>Cliches page</h1>;
+  return (
+    <main>
+      <div className='book-container'>
+        <img src='101cliches.jpg' alt='101 Common Cliches' />
+      </div>
+      <div className='form-textbox'>
+        {/*text box for enter cliche number between 1-101 
+        will need input validation
+        , button for random cliche*/}
+        <input
+          className='textbox'
+          type='text'
+          placeholder='Enter a cliche between 1-100'
+          name='cliche'
+        />
+        <input type='submit' className='btn btn-primary' />
+      </div>
+      <div className='buttons'>Click for Random Cliche</div>
+    </main>
+  );
 }
