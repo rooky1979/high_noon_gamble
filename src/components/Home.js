1d2883e628528b18a41e40becb5b9bdeb2a13bec
rooky1979 <74156579+rooky1979@users.noreply.github.com>
2021-10-29 19:58:34 +1300

    homepage setup and styling

--- src/components/Home.js
+++ src/components/Home.js
@@ -1,5 +1,37 @@
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 
 export default function Home() {
-  return <h1>Home page</h1>;
+  return (
+    <main>
+      <div className='home-container'>
+        <img src='HNGHome.png' alt='Welcome to High Noon Gamble' />
+      </div>
+      <div className='books'>
+        <NavLink to='/cliches'>
+          <img className='book' src='101cliches.jpg' alt='101 cliches' />
+        </NavLink>
+        <NavLink to='/additionalcliches'>
+          <img
+            className='book'
+            src='101additional.jpg'
+            alt='101 additional cliches'
+          />
+        </NavLink>
+        <NavLink to='/mymind'>
+          <img
+            className='book'
+            src='mindout.jpg'
+            alt='My mind is out to get me'
+          />
+        </NavLink>
+        <NavLink to='/sobercards'>
+          <img className='book' src='sobercards.png' alt='sober cards' />
+        </NavLink>
+        <NavLink to='/bbcards'>
+          <img className='book' src='bbcards.png' alt='big book cards' />
+        </NavLink>
+      </div>
+    </main>
+  );
 }
